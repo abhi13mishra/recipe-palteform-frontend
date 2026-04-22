f8793956cce02eb0a789c64792efdd44172afd1b
Abhishek <rajyadavu723@gmail.com>
2026-04-22 17:21:43 +0530

    style: UI polish (navbar line, body fix, card hover, input dark fix)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./App";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
